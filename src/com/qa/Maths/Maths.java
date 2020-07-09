ebaecee94162555871fd3b899a1b186745b5939b
pashsunuwar <psunuwar@qa.com>
2020-07-09 11:43:24 +0100

    updated maths exe

--- src/com/qa/Maths/Maths.java
+++ src/com/qa/Maths/Maths.java
@@ -1,8 +1,24 @@
 package com.qa.Maths;
+
 import java.util.Scanner;
 
 public class Maths {
-	Scanner mycalc = new Scanner(System.in);
+	Scanner myCalc = new Scanner(System.in);
 
+	switch(operator)
+	{
+	case "+" :
+		System.out.println("Adding the two operands equal to " + (firstnumber + secondnumber));
+		break;	
+	case "-" :
+		System.out.println("Subtracting the two operands equal to " + (firstnumber - secondnumber));
+		break;
+	case "*" :
+		System.out.println("Multiplying both operands equal to " + (firstnumber * secondnumber));
+		break;
+	case "/" :
+		System.out.println("Divinding both operands equal to " + (firstnumber / secondnumber));
+		break;
 	
-}
+	}
+}}
--- src/com/qa/Maths/Runner.java
+++ src/com/qa/Maths/Runner.java
@@ -1,10 +1,53 @@
 package com.qa.Maths;
 
+import java.util.Scanner;
 
 public class Runner {
 
 	public static void main(String[] args) {
-		
+
+		Scanner mycalc = new Scanner(System.in);
+
+		int firstnumber, secondnumber;
+		String operator;
+
+		System.out.println("Enter your first number: ");
+		firstnumber = mycalc.nextInt();
+
+		System.out.println("Choose Arithmetic Operator from '+ - * /': ");
+		operator = mycalc.next();
+
+		System.out.println("Enter your second number: ");
+		secondnumber = mycalc.nextInt();
+
+		switch (operator) {
+		case "+":
+			System.out
+					.println(firstnumber + " " + operator + " " + secondnumber + " = " + (firstnumber + secondnumber));
+			break;
+		case "-":
+			System.out
+					.println(firstnumber + " " + operator + " " + secondnumber + " = " + (firstnumber - secondnumber));
+			break;
+		case "*":
+			System.out
+					.println(firstnumber + " " + operator + " " + secondnumber + " = " + (firstnumber * secondnumber));
+			break;
+		case "/":
+			// exception
+			try {
+				firstnumber = 0;
+				secondnumber = 0;
+				System.out.println(
+						firstnumber + " " + operator + " " + secondnumber + " = " + (firstnumber / secondnumber));
+			} catch (ArithmeticException e) {
+				// will only execute if any Arithmetic exception occurs in the try block
+				System.out.println("You cannot divide a number by zero");
+			}
+
+			break;
+
+		}
 
 	}
 
